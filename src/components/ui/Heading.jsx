e8b0182ca7ca4880f87b1388a2c84a666ea0df4e
Bochkovskyi <unundertale1@ukr.net>
2023-09-29 16:08:51 +0300

    add new font

--- src/components/ui/Heading.jsx
+++ src/components/ui/Heading.jsx
@@ -1,3 +1,9 @@
+import { orbitron } from "@/app/fonts";
+
 export default function Heading({ children }) {
-  return <h1 className="font-bold text-2xl mb-3">{children}</h1>;
+  return (
+    <h1 className={`${orbitron.className} font-bold text-2xl mb-3`}>
+      {children}
+    </h1>
+  );
 }
