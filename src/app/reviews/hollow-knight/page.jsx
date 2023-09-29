a51ebf1715d0412a88642480c6923fc3e1cdd56f
Bochkovskyi <unundertale1@ukr.net>
2023-09-29 15:47:49 +0300

    add images

--- src/app/reviews/hollow-knight/page.jsx
+++ src/app/reviews/hollow-knight/page.jsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import Heading from "@/components/ui/Heading";
 
 export default function HollowKnightPage() {
   return (
     <div>
       <Heading>Hollow Knight</Heading>
+      <Image
+        className="rounded mb-2"
+        src="/img/hollow-knight.jpg"
+        width="640"
+        height="320"
+        alt="Hollow Knight"
+      />
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit
         adipisci voluptatibus ea, omnis eius enim ullam aliquid? Itaque eligendi
--- src/app/reviews/stardew-valley/page.jsx
+++ src/app/reviews/stardew-valley/page.jsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import Heading from "@/components/ui/Heading";
 
 export default function StardewValleyPage() {
   return (
     <div>
       <Heading>Stardew Valley</Heading>
+      <Image
+        className="rounded mb-2"
+        src="/img/stardew-valley.jpg"
+        width="640"
+        height="320"
+        alt="Stardew Valley"
+      />
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit
         adipisci voluptatibus ea, omnis eius enim ullam aliquid? Itaque eligendi
