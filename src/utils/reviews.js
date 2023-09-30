e9b27115f34cb19db16ccc4e734467743bd9d594
Bochkovskyi <unundertale1@ukr.net>
2023-09-30 20:32:14 +0300

    add getter function for all reviews

--- src/utils/reviews.js
+++ src/utils/reviews.js
@@ -1,4 +1,4 @@
-import { readFile } from "node:fs/promises";
+import { readdir, readFile } from "node:fs/promises";
 import { marked } from "marked";
 import matter from "gray-matter";
 
@@ -8,5 +8,19 @@ export async function getReview(slug) {
   const markdownHtml = marked(content);
   const { title, date, image } = data;
 
-  return { title, date, image, markdownHtml };
+  return { slug, title, date, image, markdownHtml };
+}
+
+export async function getReviews() {
+  const files = await readdir("./content/reviews");
+  let slugs = files.filter((file) => file.endsWith(".md"));
+  slugs = slugs.map((slug) => slug.slice(0, -".md".length));
+
+  const reviews = [];
+  for (const slug of slugs) {
+    const review = await getReview(slug);
+    reviews.push(review);
+  }
+
+  return reviews;
 }
