e0479275fe72cabb8009ca48b6d3cab85784b468
Bochkovskyi <unundertale1@ukr.net>
2023-09-30 21:42:39 +0300

    change about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,22 +1,10 @@
-"use client";
-
-import { useEffect } from "react";
+import Heading from "@/components/ui/Heading";
 
 export default function AboutPage() {
-  useEffect(() => {
-    window.alert("Hello, world!");
-    console.log("Hello, world!");
-  }, []);
-
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit
-        adipisci voluptatibus ea, omnis eius enim ullam aliquid? Itaque eligendi
-        sint dignissimos fuga, doloribus optio ducimus asperiores necessitatibus
-        modi commodi magni.
-      </p>
-    </div>
+    <section>
+      <Heading>About Us</Heading>
+      <p>A website created to learn Next.js.</p>
+    </section>
   );
 }
