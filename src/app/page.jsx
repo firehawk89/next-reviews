76dc48b96f1a1cdfe4938674c81ed8e4fc03ceef
Bochkovskyi <unundertale1@ukr.net>
2023-09-30 21:33:06 +0300

    display latest review

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,31 @@
 import Link from "next/link";
+import { getLatestReview } from "@/utils";
 import Heading from "@/components/ui/Heading";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const featuredReview = await getLatestReview();
+
   return (
     <section>
       <Heading>Hello, NextJS!</Heading>
       <p className="pb-3">Only the best indie games, reviewed for you.</p>
-      <div>
-        <article className="w-80 sm:w-full bg-white border rounded shadow hover:shadow-xl transition-all">
-          <Link
-            className="flex flex-col sm:flex-row"
-            href="/reviews/stardew-valley"
-          >
-            <img
-              className="rounded-t sm:rounded-l sm:rounded-r-none"
-              src="/img/stardew-valley.jpg"
-              width="320"
-              height="180"
-              alt="Stardew Valley"
-            />
-            <h2 className="font-orbitron font-semibold text-center py-1 sm:px-2">
-              Stardew Valley
-            </h2>
-          </Link>
-        </article>
-      </div>
+      <article className="w-80 sm:w-full bg-white border rounded shadow hover:shadow-xl transition-all">
+        <Link
+          className="flex flex-col sm:flex-row"
+          href={`/reviews/${featuredReview.slug}`}
+        >
+          <img
+            className="rounded-t sm:rounded-l sm:rounded-r-none"
+            src={featuredReview.image}
+            width="320"
+            height="180"
+            alt={featuredReview.title}
+          />
+          <h2 className="font-orbitron font-semibold text-center py-1 sm:px-2">
+            {featuredReview.title}
+          </h2>
+        </Link>
+      </article>
     </section>
   );
 }
