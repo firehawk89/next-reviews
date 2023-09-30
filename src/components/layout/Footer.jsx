7769020e3a338e65f11579e9432f4cfb0426a18f
Bochkovskyi <unundertale1@ukr.net>
2023-09-30 19:48:06 +0300

    change styles

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="border-t py-3 text-center text-xs">
+    <footer className="border-t py-3 text-slate-500 text-center text-xs">
       Game data and images courtesy of{" "}
       <a
         className="text-orange-800 hover:underline"
