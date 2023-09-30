1a1bf7c7dc9828285e63defda78b66e743e26db4
Bochkovskyi <unundertale1@ukr.net>
2023-09-30 20:05:37 +0300

    use gray-matter, add reusable review getter function

--- src/app/reviews/stardew-valley/page.jsx
+++ src/app/reviews/stardew-valley/page.jsx
@@ -1,28 +1,37 @@
 import { readFile } from "node:fs/promises";
 import { marked } from "marked";
+import matter from "gray-matter";
 import Image from "next/image";
 import Heading from "@/components/ui/Heading";
 
+async function getReview(slug) {
+  const markdownText = await readFile(`./content/reviews/${slug}.md`, "utf8");
+  const { content, data } = matter(markdownText);
+  const markdownHtml = marked(content);
+  const { title, date, image } = data;
+
+  return { title, date, image, markdownHtml };
+}
+
 export default async function StardewValleyPage() {
-  const markdownText = await readFile(
-    "./content/reviews/stardew-valley.md",
-    "utf8"
-  );
-  const markdownHtml = marked(markdownText);
+  const review = await getReview("stardew-valley");
 
   return (
     <article>
-      <Heading>Stardew Valley</Heading>
+      <Heading>{review.title}</Heading>
+      <time className="inline-block italic mb-2" dateTime={review.date}>
+        {review.date}
+      </time>
       <Image
         className="rounded mb-2"
-        src="/img/stardew-valley.jpg"
+        src={review.image}
         width="640"
         height="320"
         alt="Stardew Valley"
       />
       <div
         className="prose prose-slate max-w-screen-sm"
-        dangerouslySetInnerHTML={{ __html: markdownHtml }}
+        dangerouslySetInnerHTML={{ __html: review.markdownHtml }}
       />
     </article>
   );
