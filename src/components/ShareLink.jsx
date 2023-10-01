466b7827e80fb643d3a13cd3ccaaf715e5fff317
Bochkovskyi <unundertale1@ukr.net>
2023-10-01 15:44:54 +0300

    add heroicons, add share link icon

--- src/components/ShareLink.jsx
+++ src/components/ShareLink.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { LinkIcon } from "@heroicons/react/20/solid";
 
 export default function ShareLink() {
   const [clicked, setClicked] = useState(false);
@@ -13,9 +14,10 @@ export default function ShareLink() {
 
   return (
     <button
-      className="border px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700 transition-all"
+      className="flex gap-1 items-center border px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700 transition-all"
       onClick={clickHandler}
     >
+      <LinkIcon className="h-4 w-4" />
       {clicked ? "Link Copied" : "Share Link"}
     </button>
   );
