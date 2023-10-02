920aa2f6b15fac7c3242e155f031c9644538dc40
Bochkovskyi <unundertale1@ukr.net>
2023-10-02 20:17:00 +0300

    migrate to cms

--- src/utils/reviews.js
+++ src/utils/reviews.js
@@ -1,14 +1,21 @@
-import { readdir, readFile } from "node:fs/promises";
+import qs from "qs";
 import { marked } from "marked";
-import matter from "gray-matter";
+
+const CMS_URL = "http://localhost:1337";
 
 export async function getReview(slug) {
-  const markdownText = await readFile(`./content/reviews/${slug}.md`, "utf8");
-  const { content, data } = matter(markdownText);
-  const markdownHtml = marked(content);
-  const { title, description, date, image } = data;
+  const { data } = await fetchReviews({
+    filters: { slug: { $eq: slug } },
+    fields: ["slug", "title", "subtitle", "publishedAt", "body"],
+    populate: { image: { fields: "url" } },
+    pagination: { pageSize: 1, withCount: false },
+  });
+  const item = data[0];
 
-  return { slug, description, title, date, image, markdownHtml };
+  return {
+    ...toReviewObject(item),
+    body: marked(item.attributes.body, { headerIds: false, mangle: false }),
+  };
 }
 
 export async function getLatestReview() {
@@ -17,23 +24,46 @@ export async function getLatestReview() {
 }
 
 export async function getReviews() {
-  const slugs = await getSlugs();
+  const { data } = await fetchReviews({
+    fields: ["slug", "title", "subtitle", "publishedAt"],
+    populate: { image: { fields: "url" } },
+    sort: ["publishedAt:desc"],
+    pagination: { pageSize: 6 },
+  });
 
-  const reviews = [];
-  for (const slug of slugs) {
-    const review = await getReview(slug);
-    reviews.push(review);
-  }
-
-  reviews.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-  return reviews;
+  return data.map(toReviewObject);
 }
 
 export async function getSlugs() {
-  const files = await readdir("./content/reviews");
-  let slugs = files.filter((file) => file.endsWith(".md"));
-  slugs = slugs.map((slug) => slug.slice(0, -".md".length));
+  const { data } = await fetchReviews({
+    fields: ["slug"],
+    sort: ["publishedAt:desc"],
+    pagination: { pageSize: 100 },
+  });
 
-  return slugs;
+  return data.map((item) => item.attributes.slug);
+}
+
+async function fetchReviews(params) {
+  const url =
+    `${CMS_URL}/api/reviews?` +
+    qs.stringify(params, { encodeValuesOnly: true });
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`CMS returned ${response.status} for ${url}`);
+  }
+
+  return await response.json();
+}
+
+function toReviewObject(item) {
+  const { attributes } = item;
+
+  return {
+    slug: attributes.slug,
+    title: attributes.title,
+    date: attributes.publishedAt.slice(0, "yyyy-mm-dd".length),
+    image: CMS_URL + attributes.image.data.attributes.url,
+  };
 }
