1ac9be9a206f6c70904e044a25853a059147aa5d
Bochkovskyi <unundertale1@ukr.net>
2023-10-03 11:41:29 +0300

    update markup, add Image component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,36 +1,44 @@
+import Image from "next/image";
 import Link from "next/link";
-import { getLatestReview } from "@/utils";
+import { getReviews } from "@/utils";
 import Heading from "@/components/ui/Heading";
 import Container from "@/components/layout/Container";
 
 export default async function HomePage() {
-  const featuredReview = await getLatestReview();
+  const featuredReviews = await getReviews(3);
 
   return (
     <section>
       <Container>
         <Heading>Hello, NextJS!</Heading>
         <p className="pb-3">Only the best indie games, reviewed for you.</p>
-        <article className="w-80 sm:w-full bg-white border rounded shadow hover:shadow-xl transition-all">
-          <Link
-            className="flex flex-col sm:flex-row"
-            href={`/reviews/${featuredReview.slug}`}
-          >
-            <img
-              className="rounded-t sm:rounded-l sm:rounded-r-none"
-              src={featuredReview.image}
-              width="320"
-              height="180"
-              alt={featuredReview.title}
-            />
-            <div className="flex flex-col text-center sm:text-left p-2">
-              <h2 className="font-orbitron font-semibold mb-1">
-                {featuredReview.title}
-              </h2>
-              <p className="border-t">{featuredReview.description}</p>
-            </div>
-          </Link>
-        </article>
+        <ul className="flex flex-col gap-3">
+          {featuredReviews.map((review, index) => (
+            <li
+              key={review.slug}
+              className="w-80 sm:w-full bg-white border rounded shadow hover:shadow-xl transition-all"
+            >
+              <Link
+                className="flex flex-col sm:flex-row"
+                href={`/reviews/${review.slug}`}
+              >
+                <Image
+                  className="rounded-t sm:rounded-l sm:rounded-r-none"
+                  src={review.image}
+                  width="320"
+                  height="180"
+                  alt={review.title}
+                  priority={index === 0}
+                />
+                <div className="flex flex-col text-center sm:text-left p-2">
+                  <h2 className="font-orbitron font-semibold mb-1">
+                    {review.title}
+                  </h2>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </Container>
     </section>
   );
